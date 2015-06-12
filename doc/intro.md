00a32497aebf3335bb61575839893fb63367e563
Owen Lynch <oclynch888@gmail.com>
2015-06-12 00:54:15 +0000

    initial lein stuff

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to electric-keyboard
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/electric_keyboard/core_test.clj
@@ -0,0 +1,7 @@
+(ns electric-keyboard.core-test
+  (:require [clojure.test :refer :all]
+            [electric-keyboard.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
